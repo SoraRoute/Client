8580d3c3c41c8d6f8277947da44d999779171951
nishtha322 <nishthasrivastava1602@gmail.com>
2026-07-14 17:07:54 +0530

    Implement customer wishlist module

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,9 +5,11 @@ const app = express();
 app.use(express.json());
 const customerRoutes=require("./Routes/customerRoutes");
 const customerAddressRoutes=require("./Routes/customerAddressRoutes");
+const customerWishlistRoutes=require("./Routes/customerWishlistRoutes");
 
 app.use("/api/seller",sellerRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/customer-addresses", customerAddressRoutes);
+app.use("/api/customer-wishlist", customerWishlistRoutes);
 
 module.exports = app;
